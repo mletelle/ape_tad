530dff45873c22ca14ff009421068d5e780635ba
EihtSlowFlow <151700258+EihtSlowFlow@users.noreply.github.com>
2024-10-29 10:56:56 -0300

    Actualizacion tcola.c
    Me woa comer algo, alta lija

--- tcola.c
+++ tcola.c
@@ -1,1 +1,19 @@
+/// Idea inicial
+#include "tcola.h"
+//TNodo va a ser los nodos contenidos en la "lista"
+struct tNodo{
+	int dato;
+	/// Ponele que en una "lista" de 30, un nodo X está en la posicion 3
+	/// con ese criterio determinamos la posicion de cada nodo en funcion de
+	/// qElementos.
+	int posNodo;
+	NODO *sgte;
+};
+//TCola va a ser la "lista"
+struct tCola{
+	int qElementos;
+	/// Los ultimos dos serian un puntero a estructura tNodo
+	NODO ultimo;
+	NODO inicio;
+};
 
