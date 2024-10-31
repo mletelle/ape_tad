09665452feb0994d34f9f6a57a2afe6821cd1553
EihtSlowFlow <151700258+EihtSlowFlow@users.noreply.github.com>
2024-10-31 10:26:27 -0300

    Actualizacion tcola.c
    Revisen, y corrijan si ven algo que está mal

--- tcola.c
+++ tcola.c
@@ -1,4 +1,6 @@
-/// Idea inicial
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tcola.h"
 //TNodo va a ser los nodos contenidos en la "lista"
 struct tNodo{
@@ -16,4 +18,30 @@ struct tCola{
 	NODO ultimo;
 	NODO inicio;
 };
+/// Modulo/s auxiliar/es (Desconozco si se pueden usar modulos auxiliares corrijanme si estoy mal chicos)
+void asignar(NODO *C,int dato){
+	/// Asigna memoria a un nodo y setea el sgte en NULL
+	(*C) = malloc(sizeof(struct tNodo));
+	(*C)->sgte = NULL;
+	(*C)->dato = dato;
+}
+	
+/// Implementacion de 3 funcionalidades provistas por el TAD
 
+/* Agrega el elemento dato al final de la cola C. */
+void encolar(COLA *C, int dato){
+	if(vacia(*C) == 0){
+		if((*C)->ultimo->sgte == NULL){
+			asignar((*C)->ultimo->sgte,dato);
+		}
+	}
+}
+	
+/* Indica si la cola C está vacía, en cuyo caso retorna 1, 0 en otro caso. */
+int vacia(COLA C){
+	int cVacia = 1;
+	if(C != NULL){
+		cVacia = 0;
+	}
+	return cVacia;
+}
