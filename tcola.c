b35f79c895e203d6c0b1f61d02cf07296e4ef338
EihtSlowFlow <151700258+EihtSlowFlow@users.noreply.github.com>
2024-11-09 00:33:10 -0300

    Update tcola.c
    ZZZzz

--- tcola.c
+++ tcola.c
@@ -71,6 +71,8 @@ USANDO el TAD TCOLA, diseña e implementa una función llamada existe que reciba
 y retorne la posición en la que se encuentra el elemento X. Ten en cuenta que el valor podría no existir, en cuyo caso la función debe retornar -1.
 */
 int existe(COLA *C, int x){
+/// Avanza sobre la lista sumando un valor I que determina la posicion del valor buscado 'x', si se encontró la variable encontrado cambia a verdadero y retorno obtiene la "posicion" dónde
+/// quedó I, si no se encontró el valor de "retorno" nunca cambia y la funcion retorna -1. Para encontrar el valor se recorre la lista de inicio a fin
 	int retorno = -1;
 	int encontrado = 0;
 	int i = 1;
@@ -80,6 +82,7 @@ int existe(COLA *C, int x){
 		   encontrado = 1;
 		   retorno = i;
 		}
+
 		i++;
 	 	aux = aux->sgte;
 	}
