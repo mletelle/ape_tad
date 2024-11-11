b319a059161b635c0a860edf244939de0052138a
Mauro Letelle <mauro.letelle@gmail.com>
2024-11-11 10:52:45 -0300

    Update tcola.h

--- tcola.h
+++ tcola.h
@@ -1,12 +1,10 @@
 #ifndef COLA_H_INCLUDE
 #define COLA_H_INCLUDE
 
-///Estructura planteada:
-//typedef struct tNodo *NODO;
-typedef struct tCola *COLA;// importa solo el Tipo exportado? o tmb el tipo de nodo? el user interactua solo con el TIPO EXPORTADO?
-                           // Solo importa el tipo exportado y no cómo lo implementamos, osea el usuario solo sabe que la estructura es tCola y nada más, las cosas entorno a la implementacion como...
-                           //...*NODO está mal porque es parte de como el programador del TAD implementa la estructura
+//TIPO EXPORTADO
+typedef struct tCola *COLA;
 
+//MODULOS
 /* Crea una cola vacía. */
 COLA crear();
 /* Indica si la cola C está vacía, en cuyo caso retorna 1, 0 en otro caso. */
@@ -22,7 +20,8 @@ Pre-condición: cola no vacía*/
 int verPrimero(COLA C);
 /* Retorna la cantidad de elementos de la cola C. */
 int longitud(COLA C);
-/* Copia el contenido de *C1 a C2. */
+/* Copia el contenido de la cola C2 en la cola *C1. 
+   Los elementos de C2 se mantendrán inalterados. */
 void copiar(COLA *C1, COLA C2);
 /*ubica en el frente de la cola C1 al elemento qué se encuentra en la posición posX.
 posX es una posición valida en C1, comprendida entre 1 y la cantidad de elementos
