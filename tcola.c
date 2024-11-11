bf19675d70dc682c015849844d487b6406410a6b
Mauro Letelle <mauro.letelle@gmail.com>
2024-11-11 20:11:38 -0300

    Update tcola.c este compila

--- tcola.c
+++ tcola.c
@@ -28,7 +28,7 @@ COLA crear() {
 //inicializar los valores de la cola
 	colaVacia->primerNodo = NULL;
 	colaVacia->ultimoNodo = NULL;
-	colaVacia.qElementos = 0;//la cola empieza vacía
+	colaVacia->qElementos = 0;//la cola empieza vacía
 	return colaVacia;//retorna el puntero a la cola inicializada
 }
 
@@ -55,32 +55,32 @@ void encolar(COLA *C, int dato) {
 // Entra TipoExportado y se invoca funcion auxiliar con *ultimoNodo
 	tNodo *nuevoNodo;
 	asignar(&nuevoNodo, dato);
-	if(vacia(C)){
+	if(vacia(*C)){
         	//si la cola esta vacia primerNodo y ultimoNodo apuntan al nuevo nodo
-        	C->primerNodo = nuevoNodo;
-        	C->ultimoNodo = nuevoNodo;
+        	(*C)->primerNodo = nuevoNodo;
+            (*C)->ultimoNodo = nuevoNodo;
     	}else{
         //si la cola no esta vacia, linkea el ultimo nodo al nuevo y actualiza ultimoNodo
-       		C->ultimoNodo->sgte = nuevoNodo;
-        	C->ultimoNodo = nuevoNodo;
+       		(*C)->ultimoNodo->sgte = nuevoNodo;
+        	(*C)->ultimoNodo = nuevoNodo;
     	}    
-	C->qElementos++; // Incrementa la cantidad de elementos en la cola
+	(*C)->qElementos++; // Incrementa la cantidad de elementos en la cola
 }
 
 /* Desencola el primer elemento de la cola C y lo retorna en el parámetro desencolado.
 La cantidad de elementos de la cola C disminuye en 1.
 Pre-condición: cola no vacía*/
 void desencolar(COLA *C, int* desencolado) {
-    if (C->primerNodo != NULL) {
-        tNodo *temp = C->primerNodo;//se guarda el nodo a desencolar
+    if ((*C)->primerNodo != NULL) {
+        tNodo *temp = (*C)->primerNodo;//se guarda el nodo a desencolar
         *desencolado = temp->dato;//el dato del primer nodo se asigna a desencolado
-        C->primerNodo = C->primerNodo->sgte;//actualiza el primer nodo a su siguiente nodo
+        (*C)->primerNodo = (*C)->primerNodo->sgte;//actualiza el primer nodo a su siguiente nodo
 	    
-	    if (C->primerNodo == NULL) { // Si después de desencolar la cola queda vacía, se actualiza el último nodo a NULL
-            C->ultimoNodo = NULL;
+	    if ((*C)->primerNodo == NULL) { // Si después de desencolar la cola queda vacía, se actualiza el último nodo a NULL
+            (*C)->ultimoNodo = NULL;
         }
         free(temp); // Libera la memoria del nodo desencolado
-        C->qElementos--; // Disminuye la cantidad de elementos en la cola
+        (*C)->qElementos--; // Disminuye la cantidad de elementos en la cola
     }
 }
 
@@ -88,21 +88,21 @@ void desencolar(COLA *C, int* desencolado) {
 Pre-condición: cola no vacía*/
 int verPrimero(COLA C) {
 	int retorno = -1; //esto nunca debe retornarse según la pre-condicion
-	if (C.primerNodo != NULL) {
-	retorno = C.primerNodo->dato;// asigna el dato del primer nodo al retorno
+	if ((*C).primerNodo != NULL) {
+	retorno = (*C).primerNodo->dato;// asigna el dato del primer nodo al retorno
 	}
 	return retorno;
 }
 
 /* Retorna recursivamente la cantidad de elementos de la cola C. */
-int longitud(COLA *C) {//recibe el Tipo Exportado
-    return C->qElementos;//retorna lo almacenado en el campo qelementos(0(1))
+int longitud(COLA C) {//recibe el Tipo Exportado
+    return (*C).qElementos;//retorna lo almacenado en el campo qelementos
 }
 
 /* Copia el contenido de C2 a C1. */
 void copiar(COLA *C1, COLA C2) {
 	COLA nuevaCola = crear(); //crea una nueva cola para almacenar la copia
-	tNodo* aux = C2.primerNodo;//declara un nodo auxiliar y le asigna el primer nodo de la cola
+	tNodo* aux = (*C2).primerNodo;//declara un nodo auxiliar y le asigna el primer nodo de la cola
 	while (aux != NULL) { //iterar sobre los elementos de C2 y encolarlos en la nuevacola
 		encolar(&nuevaCola, aux->dato);
 		aux= aux->sgte;
@@ -114,7 +114,7 @@ void copiar(COLA *C1, COLA C2) {
 posX es una posicion valida en C1, comprendida entre 1 y la cantidad de elementos de la cola C1*/
 void llevarAlFrente(COLA *C1, int posX) {
 	if (!vacia(*C1)){
-		tNodo *actual = C1->primerNodo;
+		tNodo *actual = (*C1)->primerNodo;
 		tNodo *anterior = NULL;
 		int i = 1;//iterador
 		while (i < posX) {//recorrer la lista hasta llegar al nodo en posX
@@ -126,14 +126,14 @@ void llevarAlFrente(COLA *C1, int posX) {
 			if (anterior != NULL) {// si no es el primero, mover el nodo de la posición posX
 				anterior->sgte = actual->sgte;
 			}
-			actual->sgte = C1->primerNodo;//mover el nodo al frente
-			C1->primerNodo = actual;
+			actual->sgte = (*C1)->primerNodo;//mover el nodo al frente
+			(*C1)->primerNodo = actual;
 		}
 		if (actual->sgte==NULL) {//si el nodo movido era el último, actualizamos el último nodo
 			tNodo *aux = NULL;
-			aux = C1->ultimoNodo;
-			C1->ultimoNodo = C1->primerNodo;
-			C1->primerNodo = aux;
+			aux = (*C1)->ultimoNodo;
+			(*C1)->ultimoNodo = (*C1)->primerNodo;
+			(*C1)->primerNodo = aux;
 		}
 	}
 }
